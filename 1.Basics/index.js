ed12b5216a4c26842183ccf90e44f4d0bf580a52
Hemant <hemantkumarsonkar1@gmail.com>
2024-05-22 17:25:39 +0530

    Basic intro to modules

---
+++ 1.Basics/index.js
@@ -0,0 +1,18 @@
+// console.log("Hello world !!!");
+
+// function abc(){
+//     return 1;
+// }
+
+// const answer = abc();
+// console.log(answer, "answer");
+
+
+console.log("a");
+
+setTimeout(() => {
+    console.log("b");
+}, 0);
+
+console.log("c");
+
---
+++ 2.IntroductionToNode/2.Modules/1.HttpModule.js
@@ -0,0 +1,14 @@
+// ES6 -> import xyz from "./asda./asda./asda"
+// CommonJS -> require.
+
+
+const http = require("node:http");
+
+const server = http.createServer((req, res) => {
+    console.log("server is up and running and we can get response");
+    res.end("hello there guys");
+})
+
+server.listen(2122, () => {
+    console.log("server is listing fine", 2122);
+});
